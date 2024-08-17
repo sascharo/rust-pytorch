751aaac5bb9c14f40d2c440d21e80e821a5a147a
Sascha Robitzki <sascha@outlook.sg>
2024-08-17 19:55:46 +0800

    Refactor Cargo.toml and build.rs for stress-test

--- stress-test/build.rs
+++ stress-test/build.rs
@@ -4,5 +4,4 @@ fn main() {
     println!("cargo:rustc-link-lib=dylib=torch_cpu");
     println!("cargo:rustc-link-lib=dylib=torch_cuda");
     println!("cargo:rustc-link-lib=dylib=c10");
-    //println!("cargo:rustc-link-lib=dylib=stdc++");
 }
