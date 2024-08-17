d5054f073d6b841a31e8d5c347df8705ea16e00c
Sascha Robitzki <sascha@outlook.sg>
2024-08-17 20:10:05 +0800

    Refactor GitHub Actions workflow to update libtorch download link and add libstdc++-10-dev package

--- stress-test/build.rs
+++ stress-test/build.rs
@@ -1,7 +1,17 @@
 fn main() {
     println!("cargo:rustc-link-search=native=/usr/local/libtorch/lib");
-    println!("cargo:rustc-link-lib=dylib=torch");
-    println!("cargo:rustc-link-lib=dylib=torch_cpu");
-    println!("cargo:rustc-link-lib=dylib=torch_cuda");
-    println!("cargo:rustc-link-lib=dylib=c10");
+
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=dylib=torch");
+        println!("cargo:rustc-link-lib=dylib=torch_cpu");
+        println!("cargo:rustc-link-lib=dylib=torch_cuda");
+        println!("cargo:rustc-link-lib=dylib=c10");
+        println!("cargo:rustc-link-lib=msvcrt");
+    } else {
+        println!("cargo:rustc-link-lib=dylib=torch");
+        println!("cargo:rustc-link-lib=dylib=torch_cpu");
+        println!("cargo:rustc-link-lib=dylib=torch_cuda");
+        println!("cargo:rustc-link-lib=dylib=c10");
+        println!("cargo:rustc-link-lib=stdc++");
+    }
 }
